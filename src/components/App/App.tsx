0912f0b307da34613a69ad3d12388b3ce48a589f
Vladimir <komarov2142@gmail.com>
2019-12-04 06:50:37 +1000

    init

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      123
+    </div>
+  );
+}
+
+export default App;
