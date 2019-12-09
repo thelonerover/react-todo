2af1613d25128233037941be045a50a63216a8bd
Vladimir <komarov2142@gmail.com>
2019-12-10 08:56:02 +1000

    reset components

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,2 +1,20 @@
 import React from 'react';
-// import PropTypes from 'prop'
+
+interface TodoProps {
+    onClick: () => void,
+    completed: boolean,
+    text: string
+};
+
+const Todo = ({ onClick, completed, text }: TodoProps) => (
+    <li
+        onClick={onClick}
+        style={{
+        textDecoration: completed ? 'line-through' : 'none'
+        }}
+    >
+        {text}
+    </li>
+);
+
+export default Todo;
