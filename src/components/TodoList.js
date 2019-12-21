984c6c8c41aa416be4001283ba3a73f940a3170b
Vladimir <komarov2142@gmail.com>
2019-12-21 12:11:12 +1000

    creating containers

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Todo from './Todo';
+import PropTypes from 'prop-types'
 
 const TodoList = ({ todos, toggleTodo }) => (
     <ul>
@@ -8,5 +9,16 @@ const TodoList = ({ todos, toggleTodo }) => (
         ))}
     </ul>
 );
+
+TodoList.propTypes = {
+    todos: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            completed: PropTypes.bool.isRequired,
+            text: PropTypes.string.isRequired
+        }).isRequired
+    ).isRequired,
+    onTodoClick: PropTypes.func.isRequired
+}
 
 export default TodoList;
--- src/index.js
+++ src/index.js
@@ -3,17 +3,27 @@ import { render } from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux'
 import rootReducer from './reducers';
+import {
+    addTodo,
+    toggleTodo,
+    setVisibilityFilter,
+    VisibilityFilters
+} from './actions';
 
 const store = createStore(rootReducer);
 
-// import {
-//     addTodo,
-//     toggleTodo,
-//     setVisibilityFilter,
-//     VisibilityFilters
-// } from './actions';
+console.log(store.getState); 
 
-console.log(store.getState());
+const unsubscribe = store.subscribe(() => console.log(store.getState()));
+
+// store.dispatch(addTodo('Learn about actions'));
+// store.dispatch(addTodo('Learn about reducers'));
+// store.dispatch(addTodo('Learn about store'));
+// store.dispatch(toggleTodo(0));
+// store.dispatch(toggleTodo(1));
+// store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED));
+
+unsubscribe();
 
 render(
     <div>123123</div>
