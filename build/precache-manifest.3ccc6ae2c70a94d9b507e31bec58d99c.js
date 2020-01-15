3d1e9d9000d53c6dd8779b87d369f3d1b47593c5
Vladimir <komarov2142@gmail.com>
2020-01-15 23:26:08 +1000

    build

---
+++ build/precache-manifest.3ccc6ae2c70a94d9b507e31bec58d99c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cd0fc98b4cd2b81ed6907bb6c04ba7b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3b1ce5bfa8cc56654c39",
+    "url": "./static/css/main.7ce480af.chunk.css"
+  },
+  {
+    "revision": "bc8aa8b1a5abd82b09da",
+    "url": "./static/js/2.41c06a45.chunk.js"
+  },
+  {
+    "revision": "3b1ce5bfa8cc56654c39",
+    "url": "./static/js/main.fa298307.chunk.js"
+  },
+  {
+    "revision": "8c01499fa9f90c968b05",
+    "url": "./static/js/runtime-main.c1a4e5c6.js"
+  }
+]);
