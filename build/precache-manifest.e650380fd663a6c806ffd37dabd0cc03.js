f1a530bd3c05ee60822777f0d8d0f087755b5cd3
Vladimir <komarov2142@gmail.com>
2020-01-16 06:54:42 +1000

    build

--- build/precache-manifest.e650380fd663a6c806ffd37dabd0cc03.js
+++ build/precache-manifest.e650380fd663a6c806ffd37dabd0cc03.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9cd0fc98b4cd2b81ed6907bb6c04ba7b",
+    "revision": "3686761705da9d4758abbdb6ed7a2846",
     "url": "./index.html"
   },
   {
-    "revision": "3b1ce5bfa8cc56654c39",
-    "url": "./static/css/main.7ce480af.chunk.css"
+    "revision": "eab90e87f64044ec2a9f",
+    "url": "./static/css/main.915fb8ce.chunk.css"
   },
   {
     "revision": "bc8aa8b1a5abd82b09da",
     "url": "./static/js/2.41c06a45.chunk.js"
   },
   {
-    "revision": "3b1ce5bfa8cc56654c39",
-    "url": "./static/js/main.fa298307.chunk.js"
+    "revision": "eab90e87f64044ec2a9f",
+    "url": "./static/js/main.31da9474.chunk.js"
   },
   {
     "revision": "8c01499fa9f90c968b05",
